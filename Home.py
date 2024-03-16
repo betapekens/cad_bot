16dc3e31621c4f3d3beba11fedfb9136e5a4943f
betapekens <dikens2000@gmail.com>
2024-03-16 16:15:08 +0100

    changed placeholder

--- Home.py
+++ Home.py
@@ -56,7 +56,7 @@ for message in st.session_state.messages:
 
 client = anthropic.Client(api_key=ANYSCALE_ENDPOINT_TOKEN)
 if ANYSCALE_ENDPOINT_TOKEN is not None:
-    if prompt := st.chat_input("Try to create a 20x10 inch faceplate!"):
+    if prompt := st.chat_input("Try to create a helical gear with 10 teeth!"):
         with st.chat_message("user", avatar="🧑‍💻"):
             st.markdown(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
