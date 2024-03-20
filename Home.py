f40dcb881d063b34960d5f878bc5987fc11f5336
betapekens <44554764+betapekens@users.noreply.github.com>
2024-03-20 13:42:46 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,6 +19,7 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 ANYSCALE_ENDPOINT_TOKEN = st.secrets["ANYSCALE_ENDPOINT_TOKEN"]
 #ANYSCALE_ENDPOINT_TOKEN = st.sidebar.text_input("API KEY", "",type="password")
+subprocess.run([f"rm stl_files/obj.stl"])
 
 
 
